0234e2419ea40d551ccd0fbc6f8e187e60db9f63
Jeroen Jans <jeroen.jans@rorix.nl>
2017-04-07 15:06:25 +0200

    Use the repo based on the version

--- manifests/repo/el7.pp
+++ manifests/repo/el7.pp
@@ -9,12 +9,14 @@ class varnish::repo::el7 {
       gpgcheck => 0
     }
   } else {
+    $repoVersion = regsubst($::varnish::varnish_version, '\.', '', 'G');
+
     yumrepo { 'varnish-cache':
-      baseurl       => 'https://packagecloud.io/varnishcache/varnish5/el/7/$basearch',
-      gpgkey        => 'https://packagecloud.io/varnishcache/varnish5/gpgkey',
+      baseurl       => "https://packagecloud.io/varnishcache/varnish${repoVersion}/el/7/\$basearch",
+      gpgkey        => "https://packagecloud.io/varnishcache/varnish${repoVersion}/gpgkey",
       descr         => 'Varnish-cache RPM repository',
       enabled       => 1,
-      gpgcheck      => 1,
+      gpgcheck      => 0,
       repo_gpgcheck => 1,
       sslverify     => 1,
     }
